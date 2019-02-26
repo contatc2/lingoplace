6194f114b83c5ddadf91a102766962c1fe3321b0
Omar Berrada <berradaomar@yahoo.com>
2019-02-26 18:58:02 +0000

    Created reviews/new.html.erb page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'jack', last_name: 'doe',
+            description: 'hhhh', availability: '15/01/2087', location: 'rabat',
+            can_travel: false, interpreter: false, phone: 'hhhhh', daily_rate: 5,
+            linkedin: 'jjjjjj')
+
