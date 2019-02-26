c2feb1970cde3b21196e93097391656087e9ed72
clementine contat <clementine.contat@gmail.com>
2019-02-26 14:21:14 +0000

    updated relationships between bases

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :reviews
-  belongs_to :interpreter, class_name: ''
-  has_many :users
+  belongs_to :interpreter, class_name: 'User', foreign_key: 'interpreter_id'
 end
