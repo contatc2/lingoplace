97de501b80f0e5f45199e11ca15658beed2c674b
Henri de Navacelle <henri@navacelle.com>
2019-02-27 13:58:55 +0000

    created a seed and amended job dependencies

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :reviews
-  belongs_to :interpreter, class_name: 'User', foreign_key: 'interpreter_id'
+  belongs_to :interpreter, class_name: 'User', foreign_key: 'interpreter_id', optional: true
 
   validates :name, presence: true
   validates :description, presence: true
