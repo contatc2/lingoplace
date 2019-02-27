91af43564253aa2f7a5c3fc0e78bd46cb1f8c680
clementine contat <clementine.contat@gmail.com>
2019-02-27 11:03:04 +0000

    updated user sign up back to basic and removed users validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name description picture location phone linkedin])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name description picture location phone linkedin])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name description picture location phone linkedin])
   end
 end
