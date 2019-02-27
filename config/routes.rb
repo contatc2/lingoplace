3e49b9585cf92c7b9ef3e236c6bdbbe93071aed4
clementine contat <clementine.contat@gmail.com>
2019-02-27 18:55:05 +0000

    edited roots to nest jobs under users

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, only: %i[index show] do
     resources :interpreter_languages, except: %i[show destroy]
+    resources :jobs, only: %i[new create edit update destroy]
   end
-  resources :jobs do
+  resources :jobs, only: %i[index show] do
     resources :reviews, only: %i[index show new create]
   end
 end
