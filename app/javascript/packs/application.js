e885bb8f32016fd6cad9c4e73d7bb09dda0721c3
Omar Berrada <berradaomar@yahoo.com>
2019-03-01 12:22:05 +0000

    maded changes w/ clementine orally

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,2 @@
 import "bootstrap";
-
-const reviewInput = document.querySelector("#review_stars")
-
-const stars = document.querySelectorAll('.rating > input')
-stars.forEach(star => {
-  star.addEventListener('click', event => {
-    reviewInput.value = event.currentTarget.value
-  })
-})
+import "reviews/review.new.js"
